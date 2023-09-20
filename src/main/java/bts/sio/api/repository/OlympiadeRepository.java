b54c53b9f633f33e5d0963eea055d86e41c39a5e
unknown <adminsio@SIO301-13.sio.bts>
2023-09-20 10:26:39 +0200

    Ajout de Olympiade

---
+++ src/main/java/bts/sio/api/repository/OlympiadeRepository.java
@@ -0,0 +1,10 @@
+package bts.sio.api.repository;
+
+import bts.sio.api.model.Athlete;
+import bts.sio.api.model.Olympiade;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OlympiadeRepository extends CrudRepository<Olympiade, Long> {
+}
